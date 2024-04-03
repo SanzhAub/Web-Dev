99feb432974293948d0cf3e4f49de880682ab47c
Sanzh.aub <sa_aubakirov@kbtu.kz>
2024-04-03 20:19:28 +0600

    Lab8

---
+++ Lab8/shop_back/myvenv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\Acer\Desktop\web-dev-v2-main\web-dev-v2-main\Lab8\shop_back\myvenv\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
